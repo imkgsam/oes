d38f88e14e6b77befd8aeeb1a2ef35198ce5fd0b
13827316628 <imkgsam@163.com>
2025-07-03 19:43:19 +0800

    feat: add permission-service module

--- src/services/system/permission-service/src/main.ts
+++ src/services/system/permission-service/src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.AUTH_HTTP_PORT ?? 9201);
+  await app.listen(process.env.PERMISSION_HTTP_PORT ?? 9201);
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
       host: '127.0.0.1',
-      port: Number(process.env.AUTH_TCP_PORT ?? '9202')
+      port: Number(process.env.PERMISSION_TCP_PORT ?? '9202')
     },
   });
 
