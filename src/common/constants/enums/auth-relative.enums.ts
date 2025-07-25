37e445a31cc9370db17146696f5c8dea0f6787b1
TTH74 <imkgsam@163.com>
2025-07-26 00:35:27 +0800

    chores: update prisma schema

---
+++ src/common/constants/enums/auth-relative.enums.ts
@@ -0,0 +1,26 @@
+export const OTP_USAGES = {
+    LOGIN: "LOGIN",
+    REGISTER: "REGISTER",
+    RESET_PASSWORD: "RESET_PASSWORD",
+    MFA_VERIFY: "MFA_VERIFY"
+}
+
+export const OTP_TYPES = {
+    EMAIL: "EMAIL",
+    PHONE: "PHONE",
+    TOTP: "TOTP",
+    BACKUP_CODE: "BACKUP_CODE"
+}
+
+export const LOGIN_METHOD_TYPES = {
+    EMAIL: "EMAIL",
+    PHONE: "PHONE",
+    OAUTH_OPENID: "OAUTH_OPENID"
+}
+
+export const CREDENTIAL_TYPES = {
+    PASSWORD: "PASSWORD",
+    EMAIL_OTP: "EMAIL_OTP",
+    PHONE_OTP: "PHONE_OTP",
+    OAUTH: "OAUTH"
+}
